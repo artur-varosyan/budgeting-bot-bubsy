fdc0ac2f07de1dd20ac0f84e9d6113dc89b7581a
Artur Varosyan <artur.varosyan1@gmail.com>
2021-10-17 13:32:22 +0100

    Add temporary unknown message

--- src/app.py
+++ src/app.py
@@ -30,6 +30,8 @@ def chatbot():
             showBudget()  
         elif "spent" in words or "paid" in words:
             newExpense(words)
+        else:
+            sendMessage("Sorry I don't quite understand")
 
 def showBudget():
     print("show budget")
