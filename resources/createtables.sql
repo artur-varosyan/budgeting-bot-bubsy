d8ed58a7dfa8b3d02ba433b87804f9778f66cd46
Artur Varosyan <artur.varosyan1@gmail.com>
2022-05-21 23:21:48 +0100

    Clean up db scripts

--- resources/createtables.sql
+++ resources/createtables.sql
@@ -3,11 +3,6 @@ DROP TABLE IF EXISTS Budget;
 DROP TABLE IF EXISTS Week;
 DROP TABLE IF EXISTS Category;
 
-CREATE TABLE Week (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    budget_version INTEGER NOT NULL
-);
-
 CREATE TABLE Category (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
@@ -17,6 +12,8 @@ CREATE TABLE Budget (
     version INTEGER NOT NULL,
     category INTEGER NOT NULL,
     spend_limit FLOAT,
+    date_from DATE NOT NULL,
+    date_to DATE,
     PRIMARY KEY (version, category),
     FOREIGN KEY (category) REFERENCES Category(id)
 );
@@ -26,8 +23,7 @@ CREATE TABLE Expense (
     amount FLOAT NOT NULL,
     category INTEGER NOT NULL,
     date DATE NOT NULL,
-    week INTEGER NOT NULL,
     description VARCHAR(255),
-    FOREIGN KEY (category) REFERENCES Category(id),
-    FOREIGN KEY (week) REFERENCES Week(id)
+    recurring BOOL,
+    FOREIGN KEY (category) REFERENCES Category(id)
 );
